3bfc1b4f9cc7f96ecaf6bfa7a4355df4099cbacf
aknackd <adam@pyknotic.net>
2016-10-10 15:04:33 -0500

    Don't allow fetching species by type in /api/v1/species endpoint

--- app/Http/Controllers/Api/SpeciesController.php
+++ app/Http/Controllers/Api/SpeciesController.php
@@ -13,18 +13,11 @@ class SpeciesController extends Controller
     {
         $this->validate($request, [
             'limit' => 'numeric|between:1,100',
-            'type'  => 'string',
         ]);
 
         $limit = $request->input('limit') ?: 15;
 
-        $query = Species::query();
-
-        if ($request->has('type')) {
-            $query->where('type', $request->input('type'));
-        }
-
-        return $query->paginate($limit);
+        return Species::paginate($limit);
     }
 
     public function show(Species $species)
